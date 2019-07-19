0f7fbb34870ef9172f45b5f6ae0dd321dfbebe19
Nicolas PIGANEAU <nicolas.piganeau@ndp-systemes.fr>
2019-07-19 11:13:03 +0200

    Initial commit

---
+++ pff/pff.py
@@ -0,0 +1,255 @@
+# -*- coding: utf8 -*-
+#
+# Copyright (C) 2019 NDP Systèmes (<http://www.ndp-systemes.fr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+DEFAULT_STR_FILLER_CHAR = ' '
+DEFAULT_INT_FILLER_CHAR = '0'
+
+
+def is_numerical(typ):
+    return typ in (int, float, complex)
+
+
+class ContentOverflow(Exception):
+    """ Exception raised when the content of a `PFFCell` is larger than the cell itself """
+
+    def __init__(self, content, cell):
+        super(ContentOverflow, self).__init__()
+        self._content = content
+        self._cell = cell
+
+    def __str__(self):
+        return "Error: \"%s\" is longer than the cell %s (%d characters max)" %\
+               (self._content, self._cell.name, self._cell.size)
+
+
+class PFFWriter(object):
+    """ Object to facilitate writing data in a positional file
+
+    :param f: a file pointer, describing the file where lines will be written
+    :param lines: a collection of `PFFLine`s, which will be used to format data and write them in `self.f`
+    :param autotruncate: if True, will truncate a value to its cell size instead of raising a ContentOverflow if too
+                         long
+    """
+
+    def __init__(self, f, lines, autotruncate=False):
+        self._lines = lines
+        self._file = f
+        self.lcount = 0
+        self._autotruncate = autotruncate
+
+    def chose_line_model(self, vals):
+        """ Can be overwritten
+        Function to chose which line model (`PFFLine`) will be used to write the next line, with values `vals`
+
+        By default, always takes the first line model
+        """
+        return self._lines[0]
+
+    def writerow(self, vals, line_model=None):
+        """ Write the row with values `vals` in the associated file
+
+        :param vals: dict containing the row values
+        :param line_model: `PFFLine` to use for this row. If None, then `self.chose_line_model` is called
+        """
+        line_model = line_model or self.chose_line_model(vals)
+        self._file.write(line_model.write(vals, autotruncate=self._autotruncate) + '\n')
+        self.lcount += 1
+
+
+class PFFReader(object):
+    """ Object to facilitate writing data in a positional file
+
+    :param f: a file pointer, describing the file where lines will be written
+    :param lines: a collection of `PFFLine`s, which will be used to format data and write them in `self.f`
+    """
+
+    def __init__(self, f, lines):
+        self._lines = lines
+        self._file = f
+        self.lcount = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        line = self.readline()
+        if not line:
+            raise StopIteration
+        return line
+
+    def next(self):
+        return self.__next__()
+
+    def chose_line_model(self, line):
+        """ Can be overwritten
+        Function to chose which line model (`PFFLine`) will be used to read the next line
+
+        By default, always takes the first line model
+        """
+        return self._lines[0]
+
+    def readline(self, line_model=None):
+        """ Read a line in file `self.f`, and extract the positional data in it
+
+        :param line_model: `PFFLine` to use for this row. If None, then `self.chose_line_model` is called
+        :return: a dict associating every cell's name with their values found in line
+        """
+        line = self._file.readline()
+        if not line:
+            return None
+        line_model = line_model or self.chose_line_model(line)
+        self.lcount += 1
+        return line_model.read(line)
+
+
+class PFFLine(list):
+    """ An ordered collection of `PFFCell`s """
+
+    def __init__(self, *elems):
+        super(PFFLine, self).__init__()
+        for elem in elems:
+            if isinstance(elem, PFFCell):
+                self.append(elem)
+            elif isinstance(elem, PFFLine):
+                self.extend(elem)
+
+    def write(self, vals, autotruncate=False):
+        """ Write values in vals in the `PFFCell`s contained in this line, and outputs a str corresponding to them
+
+        :param vals: values to write
+        :return: a str representing the line filled with correct values
+        :param autotruncate: if True, will truncate a value to its cell size instead of raising a ContentOverflow if
+                             too long
+        """
+        line = ""
+        for cell in self:
+            line += cell.write(vals, autotruncate=autotruncate)
+        return line
+
+    def read(self, line):
+        """ Read a line
+
+        :param line: a str that will be read using the current line's `PFFCell`s
+        :return: a dict associating every cell's name with their values found in line
+        """
+        res = {}
+        for cell in self:
+            line = cell.read(line, res)
+        return res
+
+    def show_debug(self):
+        """ Debug view which outputs informations about the cell composing this line
+
+        :return: a string, listing each and every cell, its offset, length, type and name
+        """
+        offset = 0
+        output = u""
+        for cell in self:
+            output += "%5d %3d %s %s\n" % (offset, cell.length, cell.type, cell.name)
+            offset += cell.length
+        return output
+
+
+class PFFCell(object):
+    """ Represent a cell of Python Flat File
+
+    :param name: name of the field, used to match it in the dict
+    :param length: number of chars this cell takes
+    :param type: type of the content of this cell, useful to decide of the justifying
+    :param filler: char used to filled the blanks in this field, if the content is smaller then `length`
+    :param align: either 'l' or 'r' ; side to align the cell content (by default, 'r' if `type` is numeric, else 'l')
+    :param default: default value for this field
+    """
+
+    def __init__(self, name, length, type=str, filler=None, align=None, default=None):
+        if align not in ('l', 'r'):
+            align = is_numerical(type) and 'r' or 'l'
+        if filler is None or len(filler) != 1:
+            filler = is_numerical(type) and DEFAULT_INT_FILLER_CHAR or DEFAULT_STR_FILLER_CHAR
+        if default is None or len(default) > length:
+            default = is_numerical(type) and DEFAULT_INT_FILLER_CHAR or DEFAULT_STR_FILLER_CHAR
+        self.name = name
+        self.length = length
+        self.type = type
+        self.filler = filler
+        self.align = align
+        self.default = default
+
+    def _justify(self, content, autotruncate=False):
+        if autotruncate:
+            content = content[:self.length]
+        elif len(content) > self.length:
+            raise ContentOverflow(content, self)
+        if self.align == 'l':
+            return content.ljust(self.length, self.filler)
+        elif self.align == 'r':
+            return content.rjust(self.length, self.filler)
+
+    def write(self, vals, autotruncate=False):
+        """ Given a dict of values, takes this field's value, and formats it to fill this cell
+
+        :param vals: dict of values for the cell's line
+        :return: the corresponding field, justified
+        :param autotruncate: if True, will truncate a value to its cell size instead of raising a ContentOverflow if
+                             too long
+        """
+        content_str = str(vals.get(self.name, self.default))
+        return self._justify(content_str, autotruncate=autotruncate)
+
+    def read(self, line, dest):
+        """ Considering this line starts with the current field, reads it
+
+        :param line: line to read, supposed to start with the current cell
+        :param dest: dict of values corresponding to the current row, this cell's value will be added to it
+        :return: line, with the current field removed
+        """
+        cur_field_val = line[:self.length]
+        if self.align == 'r':
+            cur_field_val = cur_field_val.lstrip(self.filler)
+        else:
+            cur_field_val = cur_field_val.rstrip(self.filler)
+        if not cur_field_val:
+            cur_field_val = self.default
+        try:
+            cur_field_val = self.type(cur_field_val)
+        except (TypeError, ValueError):
+            pass
+        dest[self.name] = cur_field_val
+        return line[self.length:]
+
+
+class PFFBlankCell(PFFCell):
+    """ A blank cell, without any content except the filler char
+    Implements the same methods as a standard `PFFCell`
+
+    :param length: cell length
+    :param name: the field name, will be 'BLANK' if omitted
+    :param filler: char used to fill the field, by default a space
+    """
+
+    type = None
+
+    def __init__(self, length, name=None, filler=' '):
+        super(PFFBlankCell, self).__init__(name or 'BLANK', length, type(None), filler)
+
+    def write(self, vals, autotruncate=False):
+        return self.filler * self.length
+
+    def read(self, line, dest):
+        return line[self.length:]
---
+++ pff/tests/test_pff_rw.py
@@ -0,0 +1,104 @@
+# -*- coding: utf8 -*-
+#
+# Copyright (C) 2019 NDP Systèmes (<http://www.ndp-systemes.fr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import unittest
+from StringIO import StringIO
+from datetime import date
+from pff import PFFWriter, PFFReader, PFFLine, PFFCell, ContentOverflow
+
+
+class TestPFFRW(unittest.TestCase):
+    def setUp(self):
+        super(TestPFFRW, self).setUp()
+
+        # Setup
+        self.virtual_file = StringIO()
+        name_cell = PFFCell('name', 8)
+        age_cell = PFFCell('age', 3, type=int)
+        score_cell = PFFCell('score', 8, type=float, align='l', filler='#', default='418.0')
+        birth_cell = PFFCell('birthday', 10, type=date)
+        favourite_pokemon_cell = PFFCell('favourite', 128, align='r', default="Evoli")
+        town_cell = PFFCell('town', 16)
+        region_cell = PFFCell('region', 16)
+
+        # Lines
+        self.short_line = PFFLine(name_cell, age_cell, score_cell)
+        self.long_line = PFFLine(name_cell, age_cell, birth_cell, favourite_pokemon_cell)
+        self.other_line = PFFLine(town_cell, region_cell)
+
+        # Writer
+        self.writer = PFFWriter(self.virtual_file, [self.short_line, self.long_line])
+        # Reader
+        self.reader = PFFReader(self.virtual_file, [self.short_line, self.long_line])
+
+
+class TestPFFWrite(TestPFFRW):
+    def test_00_default_write_is_ok(self):
+        self.writer.writerow({'name': "Sacha", 'age': 11, 'score': 42.0})
+        self.assertEqual(self.virtual_file.getvalue(), "Sacha   01142.0####\n")
+
+    def test_01_force_line_model(self):
+        self.writer.writerow({'name': "Sacha", 'age': 11, 'birthday': date(1996, 2, 27), 'favourite': "Pikachu"},
+                             self.long_line)
+        self.assertEqual(self.virtual_file.getvalue(), "Sacha   0111996-02-27" + " " * 120 + " Pikachu\n")
+
+    def test_02_change_chose_line_model(self):
+        self.writer.chose_line_model = lambda vals: self.writer._lines[1]
+        self.writer.writerow({'name': "Sacha", 'age': 11, 'birthday': date(1996, 2, 27), 'favourite': "Pikachu"})
+        self.assertEqual(self.virtual_file.getvalue(), "Sacha   0111996-02-27" + " " * 120 + " Pikachu\n")
+
+    def test_03_default_value_when_none_given(self):
+        self.writer.writerow({'name': "Sacha", 'age': 11})
+        self.assertEqual(self.virtual_file.getvalue(), "Sacha   011418.0###\n")
+
+    def test_04_content_overflow_is_raised(self):
+        with self.assertRaises(ContentOverflow):
+            self.writer.writerow({'name': "La team Rocket", 'age': 11, 'score': 7.0}, self.short_line)
+
+    def test_05_autotruncates_prevents_content_overflow(self):
+        special_writer = PFFWriter(self.virtual_file, [self.short_line, self.long_line], autotruncate=True)
+        special_writer.writerow({'name': "La team Rocket", 'age': 11, 'score': 7.0}, self.short_line)
+        self.assertEqual(self.virtual_file.getvalue(), "La team 0117.0#####\n")
+
+
+class TestPFFRead(TestPFFRW):
+    def test_00_read_standard_line(self):
+        self.virtual_file.write("Sacha   01142.0####\n")
+        self.virtual_file.seek(0)
+        cur_dict = self.reader.readline()
+        self.assertDictEqual(cur_dict, {'name': "Sacha", 'age': 11, 'score': 42.0})
+
+    def test_01_force_line_model(self):
+        self.virtual_file.write("Sacha   0111996-02-27" + " " * 120 + " Pikachu\n")
+        self.virtual_file.seek(0)
+        cur_dict = self.reader.readline(self.long_line)
+        self.assertDictEqual(cur_dict,
+                             {'name': "Sacha", 'age': 11, 'birthday': '1996-02-27', 'favourite': "Pikachu"})
+
+    def test_02_change_chose_line_model(self):
+        self.virtual_file.write("Sacha   0111996-02-27" + " " * 120 + " Pikachu\n")
+        self.virtual_file.seek(0)
+        self.reader.chose_line_model = lambda line: self.reader._lines[1]
+        cur_dict = self.reader.readline()
+        self.assertDictEqual(cur_dict,
+                             {'name': "Sacha", 'age': 11, 'birthday': '1996-02-27', 'favourite': "Pikachu"})
+
+
+if __name__ == '__main__':
+    unittest.main()
